98db14b296bf7e95f2b9e9a4e307aed2703c6042
hoangbao0201 <hoangbao020103@gmail.com>
2023-11-29 16:08:26 +0700

    Fix "Error: Cannot find module 'passport'"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwrStratega } from '../stratega/jwt.stratega';
 import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     JwtModule.register({})
   ],
-  providers: [AuthService, UserService, JwrStratega],
+  providers: [AuthService, UserService],
   controllers: [AuthController]
 })
 export class AuthModule {}
